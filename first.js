ce5258beb3ed01b588b9a7a9320ffc08d3507bfb
Rakesh Yadav <rakesx321@gmail.com>
2025-05-19 12:50:57 +0530

    Latest changes

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-const footer = "Footer added in our Website";
-console.log(footer);
+const payment = "Integrated the payment gateway";
+console.log(payment);
+
+const upi = "Integrated the UPI";
+console.log(upi);
+
+console.log("Latest Update");
